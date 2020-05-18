3e0ebfdc9f5e9fe19062568d68f60dba591dad0f
Simon Krull <simondotunix@gmail.com>
2020-05-18 14:11:09 +0200

    Theme AddOn Deinstalliert, Template angepasst

--- redaxo/data/addons/developer/templates/Default [1]/template.php
+++ redaxo/data/addons/developer/templates/Default [1]/template.php
@@ -1,1 +1,11 @@
-REX_ARTICLE[]
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Seite</title>
+</head>
+<body>
+    
+</body>
+</html>
